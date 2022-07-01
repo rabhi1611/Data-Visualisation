32f675b1183f2981204d49ab36b5571c8825dd21
Rohit Abhishek <rabhi1611@gmail.com>
2022-07-01 14:17:44 +0530

    added frontend

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import request, response
+from django.shortcuts import render, redirect
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'app/index.html', {})
